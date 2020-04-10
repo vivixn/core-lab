4f2e803ad490fd0722b518e43d4a9a96a7977ee3
vivixn <linv927@newschool.edu>
2020-04-10 14:42:58 -0400

    json objects list class exercise

--- lab-assignments/classfiles/week11/main.js
+++ lab-assignments/classfiles/week11/main.js
@@ -2,30 +2,35 @@
 
  var animals = [
 	{
+		"emoji" : "🐓",
 		"name": "chicken",
 		"sound": "bokbok",
 		"diet": "omnivores",
 		"numberOfLegs": "two"
 	},
-	{
+	{	
+		"emoji" : "🐅",
 		"name": "tiger",
 		"sound": "rawr",
 		"diet": "carnivores",
 		"numberOfLegs": "four"
 	},
-	{
+	{	
+		"emoji" : "🐧",
 		"name": "penguin",
 		"sound": "gak",
 		"diet": "carnivores",
 		"numberOfLegs": "two"
 	},
-	{
+	{	
+		"emoji" : "🦘",
 		"name": "kangaroo",
 		"sound": "growlee",
 		"diet": "herbivores",
 		"numberOfLegs": "two"
 	},
-	{
+	{	
+		"emoji" : "🐍",
 		"name": "snake",
 		"sound": "sssss",
 		"diet": "carnivores",
@@ -33,63 +38,75 @@
 	}
 ]
 
+// for(var i=0; i<fruits.length; i++){
+// 	$('#ingredients').append("<div class='box'>" + 
+// 	fruits[i].name + "<br>" +
+// 	fruits[i].color + "<br>" +
+// 	fruits[i].flavor + "<br>" +
+// 	fruits[i].shape + "<br>" +
+// 	fruits[i].emoji + 
+// 	"</div>")
+// }
 
 
-for(var i=0; i<animals.length; i++){
-	$('#list').append("<div class='animalname'>" + animals[i].name + "</div>" + "<br>")
+for(var i=0; i<1; i++){
+	$('#list').append("<div class='chicken'>" + 
+		animals[0].emoji + "<br>" +animals[0].name + "<br>" +
+		'makes the sound'	+"<br>"+ animals[0].sound + "<br>" +
+		'they are'+"<br>"+ animals[0].diet + "<br>" +
+		'and usually have'+ "<br>"+animals[0].numberOfLegs +  ' legs'+
+		"</div>" )
 }
 
-for(var i=0; i<animals.length; i++){
-	$('#list').append("<div class='animalsound'>" + animals[i].sound + "</div>" + "<br>")
+for(var i=0; i<1; i++){
+	$('#list').append("<div class='tiger'>" + 
+		animals[1].emoji + "<br>" +animals[1].name + "<br>" +
+		'makes the sound'	+"<br>"+ animals[1].sound + "<br>" +
+		'they are'+"<br>"+ animals[1].diet + "<br>" +
+		'and usually have'+ "<br>"+animals[1].numberOfLegs +  ' legs'+
+		"</div>" )
 }
 
-for(var i=0; i<animals.length; i++){
-	$('#list').append("<div class='animaldiet'>" + animals[i].diet + "</div>" + "<br>")
+for(var i=0; i<1; i++){
+	$('#list').append("<div class='penguin'>" + 
+		animals[2].emoji + "<br>" +animals[2].name + "<br>" +
+		'makes the sound'	+"<br>"+ animals[2].sound + "<br>" +
+		'they are'+"<br>"+ animals[2].diet + "<br>" +
+		'and usually have'+ "<br>"+animals[2].numberOfLegs +  ' legs'+
+		"</div>" )
 }
-
-for(var i=0; i<animals.length; i++){
-	$('#list').append("<div class='animalnumberoflegs'>" + animals[i].numberOfLegs+ "</div>" + "<br>")
+for(var i=0; i<1; i++){
+	$('#list').append("<div class='kangaroo'>" + 
+		animals[3].emoji + "<br>" +animals[3].name + "<br>" +
+		'makes the sound'	+"<br>"+ animals[3].sound + "<br>" +
+		'they are'+"<br>"+ animals[3].diet + "<br>" +
+		'and usually have'+ "<br>"+animals[3].numberOfLegs +  ' legs'+
+		"</div>" )
 }
+for(var i=0; i<1; i++){
+	$('#list').append("<div class='snake'>" + 
+		animals[4].emoji + "<br>" +animals[4].name + "<br>" +
+		'makes the sound'	+"<br>"+ animals[4].sound + "<br>" +
+		'they are'+"<br>"+ animals[4].diet + "<br>" +
+		'and usually have'+ "<br>"+animals[4].numberOfLegs +  ' legs'+
+		"</div>" )
+}
+// for(var i=0; i<animals.length; i++){
+// 	$('#list').append("<div class='animalname'>" + animals[i].name + "</div>" + "<br>")
+// }
 
+// for(var i=0; i<animals.length; i++){
+// 	$('#list').append("<div class='animalsound'>" + animals[i].sound + "</div>" + "<br>")
+// }
 
-// var chicken = 
-// 	{
-// 		"name": "chicken"
-// 		"sound": "bokbok"
-// 		"diet": "omnivores"
-// 		"numberOfLegs": "two"
-// 	}
+// for(var i=0; i<animals.length; i++){
+// 	$('#list').append("<div class='animaldiet'>" + animals[i].diet + "</div>" + "<br>")
+// }
 
-// var tiger =
+// for(var i=0; i<animals.length; i++){
+// 	$('#list').append("<div class='animalnumberoflegs'>" + animals[i].numberOfLegs+ "</div>" + "<br>")
+// }
 
-// 	{
-// 		"name": "tiger"
-// 		"sound": "rawr"
-// 		"diet": "carnivores"
-// 		"numberOfLegs": "four"
-// 	},
-// var penguin =
-// 	{
-// 		"name": "penguin"
-// 		"sound": "gak"
-// 		"diet": "carnivores"
-// 		"numberOfLegs": "two"
-// 	},
-
-// var kangaroo =
-// 	{
-// 		"name": "kangaroo"
-// 		"sound": "growlee"
-// 		"diet": "herbivores"
-// 		"numberOfLegs": "two"
-// 	},
-// var snake =
-// 	{
-// 		"name": "snake"
-// 		"sound": "sssss"
-// 		"diet": "carnivores"
-// 		"numberOfLegs": "zero"
-// 	}
 
 
 
