f84c0c5564fdf28f6d8fc072e08ca0c2c53792df
vivixn <linv927@newschool.edu>
2020-04-17 15:33:13 -0400

    week 12 animal farm assignment

---
+++ lab-assignments/classfiles/week12/main.js
@@ -0,0 +1,39 @@
+
+
+ var animals = [
+	{
+		"emoji" : "🐓",
+		"name": "chicken",
+		"sound": "bokbok",
+		"diet": "omnivores",
+		"numberOfLegs": "two"
+	},
+	{	
+		"emoji" : "🐅",
+		"name": "tiger",
+		"sound": "rawr",
+		"diet": "carnivores",
+		"numberOfLegs": "four"
+	},
+	{	
+		"emoji" : "🐧",
+		"name": "penguin",
+		"sound": "gak",
+		"diet": "carnivores",
+		"numberOfLegs": "two"
+	},
+	{	
+		"emoji" : "🦘",
+		"name": "kangaroo",
+		"sound": "growlee",
+		"diet": "herbivores",
+		"numberOfLegs": "two"
+	},
+	{	
+		"emoji" : "🐍",
+		"name": "snake",
+		"sound": "sssss",
+		"diet": "carnivores",
+		"numberOfLegs": "zero"
+	}
+]
